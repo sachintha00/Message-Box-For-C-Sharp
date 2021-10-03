a2378583e0297c3e25016b6a109f2e5b871ba1d5
sachintha00 <sachintha0603@gmail.com>
2021-10-03 09:27:50 +0530

    updated

--- Sample Project/MessageBocC Sharp/MessageBocC Sharp/frmMessage.Designer.cs
+++ Sample Project/MessageBocC Sharp/MessageBocC Sharp/frmMessage.Designer.cs
@@ -28,13 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMessage));
+            this.btnInformation = new Guna.UI.WinForms.GunaButton();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.btnError = new Guna.UI.WinForms.GunaButton();
+            this.btnWarning = new Guna.UI.WinForms.GunaButton();
+            this.btnStop = new Guna.UI.WinForms.GunaButton();
+            this.gunaButton1 = new Guna.UI.WinForms.GunaButton();
+            this.gunaButton2 = new Guna.UI.WinForms.GunaButton();
             this.SuspendLayout();
+            // 
+            // btnInformation
+            // 
+            this.btnInformation.AnimationHoverSpeed = 0.07F;
+            this.btnInformation.AnimationSpeed = 0.03F;
+            this.btnInformation.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.btnInformation.BorderColor = System.Drawing.Color.Black;
+            this.btnInformation.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnInformation.FocusedColor = System.Drawing.Color.Empty;
+            this.btnInformation.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnInformation.ForeColor = System.Drawing.Color.White;
+            this.btnInformation.Image = ((System.Drawing.Image)(resources.GetObject("btnInformation.Image")));
+            this.btnInformation.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnInformation.Location = new System.Drawing.Point(66, 125);
+            this.btnInformation.Name = "btnInformation";
+            this.btnInformation.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnInformation.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnInformation.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnInformation.OnHoverImage = null;
+            this.btnInformation.OnPressedColor = System.Drawing.Color.Black;
+            this.btnInformation.Size = new System.Drawing.Size(206, 75);
+            this.btnInformation.TabIndex = 0;
+            this.btnInformation.Text = "gunaButton1";
+            this.btnInformation.Click += new System.EventHandler(this.btnInformation_Click_1);
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox1.HoverState.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(541, -1);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.ShadowDecoration.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.Size = new System.Drawing.Size(82, 47);
+            this.guna2ControlBox1.TabIndex = 1;
+            // 
+            // btnError
+            // 
+            this.btnError.AnimationHoverSpeed = 0.07F;
+            this.btnError.AnimationSpeed = 0.03F;
+            this.btnError.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.btnError.BorderColor = System.Drawing.Color.Black;
+            this.btnError.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnError.FocusedColor = System.Drawing.Color.Empty;
+            this.btnError.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnError.ForeColor = System.Drawing.Color.White;
+            this.btnError.Image = ((System.Drawing.Image)(resources.GetObject("btnError.Image")));
+            this.btnError.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnError.Location = new System.Drawing.Point(66, 223);
+            this.btnError.Name = "btnError";
+            this.btnError.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnError.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnError.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnError.OnHoverImage = null;
+            this.btnError.OnPressedColor = System.Drawing.Color.Black;
+            this.btnError.Size = new System.Drawing.Size(206, 75);
+            this.btnError.TabIndex = 0;
+            this.btnError.Text = "gunaButton1";
+            this.btnError.Click += new System.EventHandler(this.btnError_Click);
+            // 
+            // btnWarning
+            // 
+            this.btnWarning.AnimationHoverSpeed = 0.07F;
+            this.btnWarning.AnimationSpeed = 0.03F;
+            this.btnWarning.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.btnWarning.BorderColor = System.Drawing.Color.Black;
+            this.btnWarning.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnWarning.FocusedColor = System.Drawing.Color.Empty;
+            this.btnWarning.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnWarning.ForeColor = System.Drawing.Color.White;
+            this.btnWarning.Image = ((System.Drawing.Image)(resources.GetObject("btnWarning.Image")));
+            this.btnWarning.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnWarning.Location = new System.Drawing.Point(66, 321);
+            this.btnWarning.Name = "btnWarning";
+            this.btnWarning.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnWarning.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnWarning.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnWarning.OnHoverImage = null;
+            this.btnWarning.OnPressedColor = System.Drawing.Color.Black;
+            this.btnWarning.Size = new System.Drawing.Size(206, 75);
+            this.btnWarning.TabIndex = 0;
+            this.btnWarning.Text = "gunaButton1";
+            this.btnWarning.Click += new System.EventHandler(this.btnWarning_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.AnimationHoverSpeed = 0.07F;
+            this.btnStop.AnimationSpeed = 0.03F;
+            this.btnStop.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.btnStop.BorderColor = System.Drawing.Color.Black;
+            this.btnStop.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnStop.FocusedColor = System.Drawing.Color.Empty;
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnStop.ForeColor = System.Drawing.Color.White;
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnStop.Location = new System.Drawing.Point(322, 125);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnStop.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnStop.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnStop.OnHoverImage = null;
+            this.btnStop.OnPressedColor = System.Drawing.Color.Black;
+            this.btnStop.Size = new System.Drawing.Size(206, 75);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "gunaButton1";
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // gunaButton1
+            // 
+            this.gunaButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaButton1.AnimationSpeed = 0.03F;
+            this.gunaButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaButton1.Image = ((System.Drawing.Image)(resources.GetObject("gunaButton1.Image")));
+            this.gunaButton1.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton1.Location = new System.Drawing.Point(322, 223);
+            this.gunaButton1.Name = "gunaButton1";
+            this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton1.OnHoverImage = null;
+            this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton1.Size = new System.Drawing.Size(206, 75);
+            this.gunaButton1.TabIndex = 0;
+            this.gunaButton1.Text = "gunaButton1";
+            this.gunaButton1.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // gunaButton2
+            // 
+            this.gunaButton2.AnimationHoverSpeed = 0.07F;
+            this.gunaButton2.AnimationSpeed = 0.03F;
+            this.gunaButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaButton2.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton2.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton2.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton2.ForeColor = System.Drawing.Color.White;
+            this.gunaButton2.Image = ((System.Drawing.Image)(resources.GetObject("gunaButton2.Image")));
+            this.gunaButton2.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton2.Location = new System.Drawing.Point(322, 321);
+            this.gunaButton2.Name = "gunaButton2";
+            this.gunaButton2.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaButton2.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton2.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton2.OnHoverImage = null;
+            this.gunaButton2.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton2.Size = new System.Drawing.Size(206, 75);
+            this.gunaButton2.TabIndex = 0;
+            this.gunaButton2.Text = "gunaButton1";
+            this.gunaButton2.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // frmMessage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 570);
+            this.ClientSize = new System.Drawing.Size(624, 508);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.gunaButton2);
+            this.Controls.Add(this.gunaButton1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnWarning);
+            this.Controls.Add(this.btnError);
+            this.Controls.Add(this.btnInformation);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmMessage";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +214,14 @@
         }
 
         #endregion
+
+        private Guna.UI.WinForms.GunaButton btnInformation;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI.WinForms.GunaButton btnError;
+        private Guna.UI.WinForms.GunaButton btnWarning;
+        private Guna.UI.WinForms.GunaButton btnStop;
+        private Guna.UI.WinForms.GunaButton gunaButton1;
+        private Guna.UI.WinForms.GunaButton gunaButton2;
     }
 }
 
--- Sample Project/MessageBocC Sharp/MessageBocC Sharp/frmMessage.cs
+++ Sample Project/MessageBocC Sharp/MessageBocC Sharp/frmMessage.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SMDMessageBox;
 
 namespace MessageBocC_Sharp
 {
@@ -15,6 +16,26 @@ namespace MessageBocC_Sharp
         public frmMessage()
         {
             InitializeComponent();
+        }
+
+        private void btnInformation_Click_1(object sender, EventArgs e)
+        {
+            SMDMessage.show("Error", "Username or Password incorrect", SMDMessageBoxButtons.OK, SMDMessageBoxIcon.Information);
+        }
+
+        private void btnError_Click(object sender, EventArgs e)
+        {
+            SMDMessage.show("Error", "Username or Password incorrect", SMDMessageBoxButtons.OK, SMDMessageBoxIcon.Error);
+        }
+
+        private void btnWarning_Click(object sender, EventArgs e)
+        {
+            SMDMessage.show("Error", "Username or Password incorrect", SMDMessageBoxButtons.OK, SMDMessageBoxIcon.Warning);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            SMDMessage.show("Error", "Username or Password incorrect", SMDMessageBoxButtons.OK, SMDMessageBoxIcon.Stop);
         }
     }
 }
